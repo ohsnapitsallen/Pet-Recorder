6bb2a74d56a3747267f976ea1dad296b4013cd77
John Allen Lorena <129713164+ohsnapitsallen@users.noreply.github.com>
2023-07-06 22:11:28 +0800

    Added User Prompts

--- PetRun.py
+++ PetRun.py
@@ -6,6 +6,11 @@ class Run:
     def main():
 #Create a variable for the pet class
         petinfo = Pet()
+
 #Ask the user to input pet details
+        name = input("What is the name of your pet?: ")
+        animal_type = input("What kind of animal is your pet?: ")
+        age = int(input("How old is your pet: "))
 #Set input details to class
 #Print pet details
+#Start the function
