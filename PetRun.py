c56181d65f81db1af34af743768ade62ff1459fd
John Allen Lorena <129713164+ohsnapitsallen@users.noreply.github.com>
2023-07-06 21:42:02 +0800

    Pseudocodes

---
+++ PetRun.py
@@ -0,0 +1,7 @@
+#Import main program
+#Create a program for Run
+#Create main function
+# Create an object of the Pet class
+# Prompt the user to enter pet details
+# Set the attributes of the pet object
+# Display the pet's details
