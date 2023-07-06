2602f24fda2db71b7ec6ff7381cf3f9e51140325
John Allen Lorena <129713164+ohsnapitsallen@users.noreply.github.com>
2023-07-06 21:15:14 +0800

    Added constructor

--- PetRecorder.py
+++ PetRecorder.py
@@ -1,5 +1,9 @@
 #Create class pet
 class Pet:
 #Create a constructor
+    def __init__(self, name = ' ', animal_type = ' ', age = 0):
+        self.__name = name
+        self.__animal_type = animal_type
+        self.__age = age
 #Create setters
 #Create getters
