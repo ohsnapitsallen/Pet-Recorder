954f2f5408adf67e5acbdadf53683b98067ae12c
John Allen Lorena <129713164+ohsnapitsallen@users.noreply.github.com>
2023-07-06 21:54:39 +0800

    Added Class

--- PetRun.py
+++ PetRun.py
@@ -1,6 +1,7 @@
 #Import main program
 from Pet import*
-#Create a program for Run
+#Create a class for Run
+class Run:
 #Create main function
 # Create an object of the Pet class
 # Prompt the user to enter pet details
