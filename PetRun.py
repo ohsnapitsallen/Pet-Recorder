87d8be6cac4925c8cd6e99aa81ac3f61efaed34a
John Allen Lorena <129713164+ohsnapitsallen@users.noreply.github.com>
2023-07-06 22:17:31 +0800

    Print Statements added

--- PetRun.py
+++ PetRun.py
@@ -8,9 +8,19 @@ class Run:
         petinfo = Pet()
 
 #Ask the user to input pet details
-        name = input("What is the name of your pet?: ")
-        animal_type = input("What kind of animal is your pet?: ")
-        age = int(input("How old is your pet: "))
+        name = input("Enter the name of your pet: ")
+        animal_type = input("Enter the type of your pet (e.g., Dog, Cat, Bird): ")
+        age = int(input("Enter the age of your pet: "))
+
 #Set input details to class
+        petinfo.set_name(name)
+        petinfo.set_animal_type(animal_type)
+        petinfo.set_age(age)
+
 #Print pet details
+        print("Here are the details of your pet:")
+        print("Pet Name:", petinfo.get_name())
+        print("Animal Type:", petinfo.get_animal_type())
+        print("Age:", petinfo.get_age())
+        
 #Start the function
