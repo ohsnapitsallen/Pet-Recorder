109ddf3591e5c2c610502800ec7c0337306ee11c
John Allen Lorena <129713164+ohsnapitsallen@users.noreply.github.com>
2023-07-06 21:23:55 +0800

    Added Setters

--- PetRecorder.py
+++ PetRecorder.py
@@ -6,4 +6,12 @@ class Pet:
         self.__animal_type = animal_type
         self.__age = age
 #Create setters
+    def set_name(self, name):
+        self.__name = name
+
+    def set_animal_type(self, animal_type):
+        self.__animal_type = animal_type
+
+    def set_age(self, age):
+        self.__age = age
 #Create getters
